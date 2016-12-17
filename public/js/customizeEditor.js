368ff7e663f8a76b59ec55d55a2e1c61bc745c5d
Daniel Caffrey <scandox@gmail.com>
2016-12-17 15:05:59 +0000

    Added zero length validation for annotation comments

--- public/js/customizeEditor.js
+++ public/js/customizeEditor.js
@@ -1,11 +1,29 @@
 "use strict";
 
+var $ = require('jquery');
+
 /* An extension solely for overriding default behaviour
    of Annotator Editor
 
 */
 module.exports = function(e) {
-  e._onTextareaKeydown = TextareaKeydown.bind(this);
+  e._onTextareaKeydown = TextareaKeydown.bind(e);
+  e._original_submit = e.submit;
+  e.submit = submit.bind(e);
+}
+
+function submit() {
+  var textarea= $(this.element).find("textarea").first();
+  var text = textarea.val();
+  if (text.length === 0) {
+    textarea.parent().children('p').remove();
+    textarea.parent().append('<p style="color:red">Speak your mind, consequences be damned.</p>');
+    textarea.on('keydown', function() {
+      textarea.parent().children('p').remove();
+    });
+  }else{
+    return this._original_submit();
+  }
 }
 
 // Editor textarea keyboard shortcuts.
